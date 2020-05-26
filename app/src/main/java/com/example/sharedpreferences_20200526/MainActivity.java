50f62d15b04ed86866b7deeeecfec27dfff76185
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-26 10:53:09 +0900

    checkBox의 check 여부 확인

--- app/src/main/java/com/example/sharedpreferences_20200526/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences_20200526/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.sharedpreferences_20200526.databinding.ActivityMainBinding;
 
@@ -13,10 +15,23 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
     }
 
     @Override
     public void setupEvents() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isIdSave = binding.idSaveCheckBox.isChecked();
+                if(isIdSave){
+                    Toast.makeText(mContext, "아이디 저장 필요", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, "아이디 저장 불필요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
