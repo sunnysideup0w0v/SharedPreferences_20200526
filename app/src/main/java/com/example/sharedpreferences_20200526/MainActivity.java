4735546b248484f85e58c792f75d13aa9b0e4e39
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-26 11:19:29 +0900

    아이디 저장/ 조회 실제 활용

--- app/src/main/java/com/example/sharedpreferences_20200526/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences_20200526/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.sharedpreferences_20200526.databinding.ActivityMainBinding;
+import com.example.sharedpreferences_20200526.utils.ContextUtil;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding binding;
@@ -26,7 +27,9 @@ public class MainActivity extends BaseActivity {
             public void onClick(View v) {
                 boolean isIdSave = binding.idSaveCheckBox.isChecked();
                 if(isIdSave){
-                    Toast.makeText(mContext, "아이디 저장 필요", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(mContext, "아이디 저장 필요", Toast.LENGTH_SHORT).show();
+                    String inputId = binding.emailEdt.getText().toString();
+                    ContextUtil.setUserId(mContext, inputId);
                 } else {
                     Toast.makeText(mContext, "아이디 저장 불필요", Toast.LENGTH_SHORT).show();
                 }
@@ -37,6 +40,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        binding.emailEdt.setText(ContextUtil.getUserId(mContext));
     }
 }
